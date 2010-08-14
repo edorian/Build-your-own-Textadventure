5585852b2bb86b8f7e20ca415f1b94b5d5e3dbff
Gregor Müllegger <gregor@muellegger.de>
2010-08-14 12:15:17 +0200

    Adding some user profile goodness.

---
+++ src/website/profile/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User')
+
+    def __unicode__(self):
+        return self.user.username
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'profile', (), {}
+
+
+def create_profile_for_user(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+def sync_profiles(sender, created_models, **kwargs):
+    if Profile in created_models:
+        for user in User.objects.all():
+            Profile.objects.get_or_create(user=user)
+
+
+models.signals.post_save.connect(create_profile_for_user, sender=User)
+models.signals.post_syncdb.connect(sync_profiles)
--- src/website/urls.py
+++ src/website/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'website.views.index', name='index'),
+    url(r'^profile/$', 'website.profile.views.profile', name='profile'),
     url(r'^', include('registration.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
