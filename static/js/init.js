c0948e169c99bd906b2e0de25be703e9a3b11ca6
Gregor Müllegger <gregor@muellegger.de>
2010-08-15 01:13:13 +0200

    Dismiss messages on click

--- static/js/init.js
+++ static/js/init.js
@@ -2,3 +2,9 @@ var wmd_options = {
     output: "Markdown",
     buttons: "bold italic | blockquote image | ol ul heading hr",
 };
+
+$(function () {
+    $('#messages li').live('click', function () {
+        $(this).slideUp(1000, function () { $(this).remove(); });
+    });
+});
