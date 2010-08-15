9e107d590a38516978d9b51cc4104dab66ec4231
Gregor Müllegger <gregor@muellegger.de>
2010-08-15 23:54:24 +0200

    Custom 404 and 500 handlers.

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,6 +1,18 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def handler404(request):
+    return render_to_response('404.html', {
+    }, context_instance=RequestContext(request))
+
+
+def handler500(request):
+    return render_to_response('500.html', {
+    }, context_instance=RequestContext(request))
 
 
 admin.autodiscover()
