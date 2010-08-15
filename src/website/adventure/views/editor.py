bb4677afc7b2ae813cbaeecbb838571c673966e9
Gregor Müllegger <gregor@muellegger.de>
2010-08-16 00:41:41 +0200

    Adding messages for when the location is saved.

--- src/website/adventure/views/editor.py
+++ src/website/adventure/views/editor.py
@@ -88,10 +88,15 @@ def location_form(request, adventure_id, location_id=None):
         location = None
     else:
         location = get_object_or_404(queryset, pk=location_id)
+    create = location is None
     if request.method == 'POST':
         form = LocationForm(request.POST, request.FILES, instance=location, adventure=adventure)
         if form.is_valid():
             location = form.save()
+            if create:
+                messages.success(request, u"You have successfully created the location.")
+            else:
+                messages.success(request, u"You have successfully changed the location.")
             if '_continue' in request.POST:
                 return HttpResponseRedirect(
                     reverse('location-edit', kwargs={'adventure_id': adventure.pk, 'location_id': location.pk}))
@@ -105,7 +110,7 @@ def location_form(request, adventure_id, location_id=None):
         form = LocationForm(instance=location, adventure=adventure)
 
     context = {
-        'create': location is None,
+        'create': create,
         'object': location or Location(adventure=adventure),
         'form': form,
         'adventure': adventure,
