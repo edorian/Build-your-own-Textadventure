5b5311cbec7a644bed1a95ab5c659d53b34ab41f
Gregor Müllegger <gregor@muellegger.de>
2010-08-15 16:55:27 +0200

    Fixing bug in show graph tag.

--- src/website/adventure/templatetags/adventure_tags.py
+++ src/website/adventure/templatetags/adventure_tags.py
@@ -1,12 +1,17 @@
 from django import template
+from website.adventure.models import Graph
 
 register = template.Library()
 
 
 @register.inclusion_tag('adventure/graph/graph.html')
 def adventure_graph(adventure, css_class=None):
+    try:
+        graph = adventure.graph
+    except Graph.DoesNotExist:
+        graph = None
     return {
         'css_class': css_class,
         'adventure': adventure,
-        'graph': adventure.graph,
+        'graph': graph,
     }
