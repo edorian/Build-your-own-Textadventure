cd0f07cb5b4a5d2141a3addb058d1d46d07a0f36
Volker Dusch <github@wallbash.com>
2010-08-15 23:25:43 +0200

    Popular adventures and random adventures on startpage delayed, 'saved' query

--- src/website/views.py
+++ src/website/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from website.adventure.models import Adventure
+from django.db.models import Avg, Count
 
 def index(request):
-    return render_to_response('index.html', {
-    }, context_instance=RequestContext(request))
+#    adventures = Adventure.objects.filter(published=True)
+#    adventures = adventures.annotate(avg_rating=Avg('rating__rating'))
+#    adventures = adventures.annotate(ratings=Count('rating__rating'))
+
+#    adventures.filter(ratings__gte=Adventure.RATINGS_REQUIRED_TO_CARE).order_by('avg_rating')[:5]
+    
+    context = {
+#        "popular_adventures": adventures,
+#        "random_adventure": 
+    }
+
+    return render_to_response('index.html', context,
+        context_instance=RequestContext(request))
 
 
 def about(request):
