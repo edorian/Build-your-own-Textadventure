254d68afdfd20790b763bae50e3528170e0e7918
Gregor Müllegger <gregor@muellegger.de>
2010-08-15 16:40:56 +0200

    Tweaking graph css and displaying next to adventure form.

--- src/website/adventure/templatetags/adventure_tags.py
+++ src/website/adventure/templatetags/adventure_tags.py
@@ -4,8 +4,9 @@ register = template.Library()
 
 
 @register.inclusion_tag('adventure/graph/graph.html')
-def adventure_graph(adventure):
+def adventure_graph(adventure, css_class=None):
     return {
+        'css_class': css_class,
         'adventure': adventure,
         'graph': adventure.graph,
     }
